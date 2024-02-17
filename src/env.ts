e9a35ad0933edc62748b451263506b39818b63e4
Leonardo M. Primieri <leo.primieri@gmail.com>
2024-02-17 14:59:03 -0300

    chore: react query

--- src/env.ts
+++ src/env.ts
@@ -1,8 +1,8 @@
 import z from 'zod'
 
 const envSchema = z.object({
-  PORT: z.string().default('3000'),
-  API_URL: z.string().url(),
+  VITE_PORT: z.string().default('3000'),
+  VITE_API_URL: z.string().url(),
 })
 
-export const env = envSchema.parse(process.env)
+export const env = envSchema.parse(import.meta.env)
