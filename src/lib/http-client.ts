2ba157d10d7e725cf27d85358ef69d3e9b5ef56d
Leonardo M. Primieri <leo.primieri@gmail.com>
2024-02-18 20:58:21 -0300

    feat: skeleton

--- src/lib/http-client.ts
+++ src/lib/http-client.ts
@@ -6,3 +6,14 @@ export const httpClient = axios.create({
   baseURL: env.VITE_API_URL,
   withCredentials: true,
 })
+
+if (env.VITE_ENABLE_API_DELAY) {
+  httpClient.interceptors.response.use(
+    (response) =>
+      new Promise((resolve) => {
+        setTimeout(() => {
+          resolve(response)
+        }, 1000)
+      }),
+  )
+}
